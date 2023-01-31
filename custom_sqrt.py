a5c837f48d9620163b3d14c44892a816359c94d7
WillBAnders <WillBAnders@gmail.com>
2023-01-31 17:56:29 -0500

    Implement custom_sqrt and change input type to float (Resolves #1)

--- custom_sqrt.py
+++ custom_sqrt.py
@@ -3,12 +3,13 @@ import math
 
 
 def custom_sqrt(number):
-    # TODO: implementation
+    if number < 0:
+        return -math.sqrt(-number)
     return math.sqrt(number)
 
 
 parser = argparse.ArgumentParser(description="Calculate the square root of a number")
-parser.add_argument("number", type=int)
+parser.add_argument("number", type=float)
 args = parser.parse_args()
 
 print(custom_sqrt(args.number))
